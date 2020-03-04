cd5e14ddb94a9eab483d7aec1858da75d48e7328
Oz Ramos <checkboxoz@gmail.com>
2020-03-03 20:26:06 -0800

    #2 Shoot with Handsfree

--- src/Bird.js
+++ src/Bird.js
@@ -13,8 +13,8 @@ class Bird {
 
     // Random speeds
     this.speed = {
-      x: 4 + Math.random() * 8,
-      y: 4 + Math.random() * 8
+      x: 3 + Math.random() * 4,
+      y: 3 + Math.random() * 4
     }
 
     // 1 for right, -1 for left
@@ -33,34 +33,104 @@ class Bird {
       // right
       spritesheet.get(270, 0, this.width, this.height),
       spritesheet.get(360, 0, this.width, this.height),
-      spritesheet.get(450, 0, this.width, this.height)
+      spritesheet.get(450, 0, this.width, this.height),
+
+      // flying up
+      spritesheet.get(0, 180, this.width, this.height),
+      spritesheet.get(90, 180, this.width, this.height),
+      spritesheet.get(180, 180, this.width, this.height),
+
+      // Shot
+      spritesheet.get(0, 270, this.width, this.height),
+
+      // dying
+      spritesheet.get(90, 270, this.width, this.height),
+      spritesheet.get(180, 270, this.width, this.height)
     ]
     this.animFrame = 0
     this.animInterval = setInterval(() => {
       this.animFrame++
     }, 250)
+
+    // How long this bird stays on the screen
+    this.timeTillFlee = Math.floor(Math.random() * 6) + 40
+    this.timeTillFleeInterval = setInterval(() => {
+      --this.timeTillFlee
+      if (!this.timeTillFlee && !this.isDying) {
+        this.flee()
+      }
+    }, 1000)
   }
 
+  /**
+   * Draw the sprite
+   */
   draw() {
     this.animMod = this.dir.x > 0 ? 0 : 3
-    image(this.image[(this.animFrame % 3) + this.animMod], this.x, this.y)
+
+    if (!this.isFleeing && !this.isDying) {
+      image(this.image[(this.animFrame % 3) + this.animMod], this.x, this.y)
+    } else if (this.isDying) {
+      image(this.image[(this.animFrame % 2) + 10], this.x, this.y)
+    } else {
+      image(this.image[(this.animFrame % 3) + 6], this.x, this.y)
+    }
   }
 
+  /**
+   * Update coordinates
+   */
   update() {
     this.x += this.speed.x * this.dir.x
     this.y += this.speed.y * this.dir.y
 
-    if (this.x < 0) {
-      this.dir.x = 1
-    } else if (this.x + this.width > width) {
-      this.dir.x = -1
-    }
-    if (this.y < 0) {
-      this.dir.y = 1
-    } else if (this.y + this.height > 600) {
-      this.dir.y = -1
+    if (!this.isFleeing && !this.isDying) {
+      if (this.x < 0) {
+        this.dir.x = 1
+      } else if (this.x + this.width > width) {
+        this.dir.x = -1
+      }
+      if (this.y < 0) {
+        this.dir.y = 1
+      } else if (this.y + this.height > 600) {
+        this.dir.y = -1
+      }
     }
 
     this.draw()
+  }
+
+  /**
+   * Leave the scene
+   */
+  flee() {
+    this.isFleeing = true
+    this.speed.x = 0
+    this.speed.y = -8
+    this.dir.y = 1
+
+    // this.destroy()
+  }
+
+  /**
+   * Fall into the ground
+   */
+  die() {
+    this.isDying = true
+    this.speed.x = 0
+    this.speed.y = 8
+    this.dir.y = 1
+
+    console.log('die')
+    // this.destroy()
+  }
+
+  /**
+   * Free memory
+   */
+  destroy() {
+    clearInterval(this.timeTillFleeInterval)
+    clearInterval(this.animInterval)
+    this.shouldDelete = true
   }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -2,21 +2,52 @@ let birds = []
 let spritesheet
 handsfree = new Handsfree({ weboji: true })
 
-preload = function() {
+function preload() {
   spritesheet = loadImage('/assets/spritesheet.png')
 }
 
-setup = function() {
+function setup() {
   let canvas = createCanvas(1280, 769)
   canvas.parent('sketch-container')
   birds.push(new Bird())
   birds.push(new Bird())
 }
 
-draw = function() {
+function draw() {
   background('#3cbcfc')
 
+  for (let i = birds.length - 1; i >= 0; i--) {
+    birds[i].update()
+
+    if (birds[i].shouldDelete) {
+      birds.splice(i, 1)
+    }
+  }
+}
+
+/**
+ * Shoot with mouse
+ */
+function mouseClicked() {
+  checkIfShot(mouseX, mouseY)
+}
+
+/**
+ * Shoot with Handsfree
+ */
+handsfree.use('shoot', ({ weboji }) => {
+  if (weboji.pointer.state === 'mouseDown') {
+    checkIfShot(weboji.pointer.x, weboji.pointer.y)
+  }
+})
+
+/**
+ * Checks if we shot a bird
+ */
+function checkIfShot(x, y) {
   birds.forEach(bird => {
-    bird.update()
+    if (x > bird.x && x < bird.x + bird.width && y > bird.y && y < bird.y + bird.height) {
+      bird.die()
+    }
   })
 }
